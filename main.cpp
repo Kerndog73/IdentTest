445d5f0d1ef31544d4980eec50c8cf837b5f834e
Kerndog73 <kerndog73@gmail.com>
2019-02-12 20:25:46 +1030

    Test __FUNCSIG__

--- main.cpp
+++ main.cpp
@@ -7,6 +7,21 @@ void *dynlib1_tu();
 void *stalib0_tu();
 void *stalib1_tu();
 
+namespace ns {
+
+template <typename Type>
+void print() {
+#ifdef __GNUC__
+  std::cout << __PRETTY_FUNCTION__ << '\n';
+#elif defined(_MSC_VER)
+  std::cout << __FUNCSIG__ << '\n';
+#else
+  std::cout << "Not supported\n";
+#endif
+}
+
+}
+
 int main() {
   std::cout << "exe tu0 " << reinterpret_cast<void *>(getID<int>()) << '\n';
   std::cout << "exe tu1 " << exec_tu() << '\n';
@@ -14,4 +29,7 @@ int main() {
   std::cout << "dll1    " << dynlib1_tu() << '\n';
   std::cout << "lib0    " << stalib0_tu() << '\n';
   std::cout << "lib1    " << stalib1_tu() << '\n';
+  ns::print<int>();
+  ns::print<class HelloWorld>();
+  ns::print<std::ostream>();
 }
